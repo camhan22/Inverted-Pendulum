9e59a8e5ea755d6759d14e6544cb7c4fd074b54d
camhan22 <hansoncameron18@yahoo.com>
2020-05-27 16:54:55 -0400

    Update Controller_Code.ino

--- Controller_Code/Controller_Code.ino
+++ Controller_Code/Controller_Code.ino
@@ -26,19 +26,19 @@ int Angle_Sensor_Pin = A5;
 //SYSTEM PARAMETERS//
 int Max_Sensor_Value = 1023;// Max sensor value at system limits
 int Min_Sensor_Value = 0;// Min sensor value at system limts
-int Sample_Period = 500;// Time between samples
+int Sample_Period = 100;// Time between samples
 unsigned long Current_Time = 0;
 unsigned long Last_Time = 0;
 //END SYSTEM PARAMETERS//
 
 //CONTROLLER PARAMETERS//
-double Control_Signal = 0;
+double Control_Signal = 100;
 int Previous_Value;// Holds the previous value of the sensor during the swing
 bool is_Controlled = false;// Variable to hold whether the system should swing or control the arm
 bool is_Updated = false;// Tells if the swing direction and speed need to be updated
 //By default it should swing since it will be at the bottom of the arc
-int Swing_Limit = 10;// Variable to hold the value at which the controller will switch to controlled mode
-int Swing_Rate = 25;// Dictates how fast the system will get to the controlling point
+int Swing_Limit = 75;// Variable to hold the value at which the controller will switch to controlled mode
+int Swing_Rate = 5;// Dictates how fast the system will get to the controlling point
 Pid Controller(100, 'm', (Max_Sensor_Value+Min_Sensor_Value)/2, 0);// Create the controller
 //END CONTROLLER PARAMETERS//
 
@@ -63,15 +63,15 @@ void setup() {
 	if(Current_Time - Last_Time >= Sample_Period)
 	{
 		//CHECK DIRECTION AND SEE IF UPDATE IS NEEDED//
-		if(Control_Signal > 0){// If we are rotating in the clockwise direction	
-			if(analogRead(Angle_Sensor_Pin) - Previous_Value < 0 && abs(analogRead(Angle_Sensor_Pin) - Previous_Value) < 200){
+		if(Control_Signal < 0){// If we are rotating in the clockwise direction
+			if(analogRead(Angle_Sensor_Pin) - Previous_Value + 5 < 0 && analogRead(Angle_Sensor_Pin) < 1023/2){
 				// If the new value is less than the old value which can only happen when the direction reverses due to gravity, reverse the direction.
 				//Also check to see that the difference is not greater than 200 which happens at the bottom of the swing due to crossover
 				is_Updated = false;
 			}
 		}
 		else{// Counter-clockwise direction
-			if(analogRead(Angle_Sensor_Pin) - Previous_Value > 0 && abs(analogRead(Angle_Sensor_Pin) - Previous_Value) < 200){
+			if(analogRead(Angle_Sensor_Pin) - Previous_Value> 0 - 5 && analogRead(Angle_Sensor_Pin) > 1023/2){
 				is_Updated = false;// We have given it new commands and we shoudl do them
 			}
 		}
