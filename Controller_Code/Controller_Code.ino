22911dafee05080d8628ccf42a805445c0aea843
camhan22 <hansoncameron18@yahoo.com>
2020-06-03 04:38:43 -0400

    Added Mode Switch
    Added mode switch that can change between a static reference, moving reference or track an external voltage.

--- Controller_Code/Controller_Code.ino
+++ Controller_Code/Controller_Code.ino
@@ -15,11 +15,17 @@
  */
 
 //PIN DEFINITIONS//
-int PWM_Pin = 9;
+int PWM_Pin = 11;
 int IN3_Pin = 2;
 int IN4_Pin = 3;
 int Power_Pin = 13;// Pin used to provide power to the H-Bridge for convience
+int Power_Pin_Pot = 4;// Pin used to give 5V to potentiometer
+int Pot_Ground = 5;
+int Mode_Pin_1 = 7;//Pin to determine the Mode the stem is is (Red wire)
+int Mode_Pin_2 = 8;//Pin to determine what Mode the system is in (Blue Wire)
 int Angle_Sensor_Pin = A5;
+int Pot_Pin = A4;
+int Track_Pin = A3;
 //END PIN DEFINITIONS//
 
 //SYSTEM PARAMETERS//
@@ -28,6 +34,7 @@ int Min_Sensor_Value = 0;// Min sensor value at system limts
 int Sample_Period = 50;// Time between samples
 unsigned long Current_Time = 0;
 unsigned long Last_Time = 0;
+int Mode = 0;
 //END SYSTEM PARAMETERS//
 
 //CONTROLLER PARAMETERS//
@@ -38,27 +45,69 @@ int Previous_Value;// Holds the previous value of the sensor during the swing
 bool is_Controlled = false;// Variable to hold whether the system should swing or control the arm
 bool is_Updated = false;// Tells if the swing direction and speed need to be updated
 //By default it should swing since it will be at the bottom of the arc
-int Swing_Limit = 10;// Variable to hold the value at which the controller will switch to controlled mode
+int Swing_Limit = 10;// Variable to hold the value at which the controller will switch to controlled Mode
 int Swing_Rate = 5;// Dictates how fast the system will get to the controlling point
-Pid Controller(100, 'm', Reference, 1.25, 0.40, 0.15);// Create the controller
+Pid Controller(100, 'm', Reference, 1.5, 0.40, 0.2);// Create the controller
 //END CONTROLLER PARAMETERS//
 
 //FUNCTION PROTOTYPES//
 void SetSpeed(double cs);
 void SetDirection(double cs);
 double MapDouble(int x, int in_min, int in_max, int out_min, int out_max);
+bool ModeChanged();
+void(* resetFunc) (void) = 0;// Needed to reset the arduino by software
 //END FUNCTION PROTOTYPES//
 
 void setup() {
+  delay(2000);
   //Setup the I/O pins
   pinMode(Power_Pin, OUTPUT);
   digitalWrite(Power_Pin, HIGH);// Set the pin to output 5v for H_Bridge
+  pinMode(Power_Pin_Pot, OUTPUT);
+  digitalWrite(Power_Pin_Pot, HIGH);// Set the pin to output 5v for Potentiometer
+  pinMode(Pot_Ground, OUTPUT);
+  digitalWrite(Pot_Ground, LOW);// Set the pin to output 5v for Potentiometer
+  
   pinMode(PWM_Pin, OUTPUT);
   pinMode(Angle_Sensor_Pin, INPUT);
   pinMode(IN3_Pin, OUTPUT);
   pinMode(IN4_Pin, OUTPUT);
-  Serial.begin(9600);// Start serial communication for debugging
-  
+
+  pinMode(Mode_Pin_1,INPUT_PULLUP);
+  pinMode(Mode_Pin_2,INPUT_PULLUP);
+
+  //READ AND SELECT MODE//
+  if(!digitalRead(Mode_Pin_1) && !digitalRead(Mode_Pin_2))
+  {
+    Mode = 1;// Follows the potentiometer reading
+  }
+  else if(!digitalRead(Mode_Pin_1) && digitalRead(Mode_Pin_2))
+  {
+    Mode = 0;// Goes to the pre-defined reference value stored
+  }
+  else
+  {
+    Mode = 2;// Follows an external voltage source
+  }
+  //READ AND SELECT MODE//
+
+  //REFERENCE SETUP BASED ON Mode//
+  switch (Mode)
+  {
+    case 1:
+      Controller.setReference(analogRead(Pot_Pin));
+      break;
+
+    case 2:
+      Controller.setReference(analogRead(Track_Pin));
+      break;
+      
+    default:
+      break;
+  }
+  //END REFERENCE SETUP BASED ON Mode//
+
+  //START-UP SWING//
   while(!is_Controlled)// While the system swings
   {
 	  Current_Time = millis();
@@ -78,9 +127,8 @@ void setup() {
 				  is_Updated = false;// We have given it new commands and we shoudl do them
 			  }
 		  }
-		  //END CHECK DIRECTION AND SEE IF UPDATE IS NEEDED//
 
-      if(millis()-Last_Time > 500){// Only update the output only twice a second to avoid large torques applied to belt
+      if(millis()-Last_Time > 200){// Only update the output only twice a second to avoid large torques applied to belt
 		    //UPDATE THE SWING//
 		    if(!is_Updated){// Only do this if we need to change direction
 			    Control_Signal+= (Control_Signal > 0) ? Swing_Rate : -1*Swing_Rate;
@@ -90,17 +138,14 @@ void setup() {
 			    SetSpeed(Control_Signal);
 			    is_Updated = true;// Tell the controller the state has been updated
 		    }
-		    //END UPDATE THE SWING//
-		
 		    //DO THESE EVERY SAMPLE//
         Last_Time = Current_Time;
 		    Previous_Value = Current_Value;// Update the previous value
 		  }
 	  }
 
-    int Value = analogRead(Angle_Sensor_Pin);
-    Serial.println(Value);
     // Check this every time through the loop, we do not want to miss this 
+    int Value = analogRead(Angle_Sensor_Pin);
     if (Value > Reference-Swing_Limit && Value < Reference+Swing_Limit){// Check to see if it is within the limit for PID to take over
     is_Controlled = true;// If so, then we will allow the PID to take over
     SetSpeed(0);// Stop the motor
@@ -108,19 +153,35 @@ void setup() {
 	  }
   //If we have not gotten a sample, continue to do the same thing. i.e. change nothing
   }
+  //END START-UP SWING//
 }
 
 void loop() {
-
   Current_Time = millis();  
   if(Current_Time - Last_Time >= Sample_Period)
   {
+    if(ModeChanged())// Check if the Mode changed, if so, reset the arduino
+    {
+      SetSpeed(0);//Turn the PWM off to allow the arm to fall
+      resetFunc();// Reset the Arduino
+    }
+    
+    switch (Mode)
+    {
+      case 1:
+        Controller.setReference(map(analogRead(Pot_Pin),0,1023,1023,0));
+        break;
+      case 2:
+        Controller.setReference(analogRead(Track_Pin));
+        break; 
+      default:// Don't do anything
+        break;
+    }
     Controller.SetInput(analogRead(Angle_Sensor_Pin));// Read the sensor and put it into the controller
     Control_Signal = Controller.ControlValue();// Calculate the control value of the system
     SetSpeed(Control_Signal);// Set the speed of the controller
     SetDirection(Control_Signal);// Set the dirction of the controller
 	  Last_Time = Current_Time;// Update the last time the loop exectuted
-   Serial.println(Control_Signal);
   }
   //If we have not gotten a sample, continue to do the same thing. i.e. change nothing
 }
@@ -148,4 +209,28 @@ void SetDirection(double cs)
 double MapDouble(int x, int in_min, int in_max, int out_min, int out_max){
  return ((double)x - (double)in_min) * ((double)out_max - (double)out_min) / ((double)in_max - (double)in_min) + (double)out_min;
  }
+bool ModeChanged()
+{
+  int currentmode = Mode;
+  if(!digitalRead(Mode_Pin_1) && !digitalRead(Mode_Pin_2))
+  {
+    Mode = 1;// Follows the potentiometer reading
+  }
+  else if(!digitalRead(Mode_Pin_1) && digitalRead(Mode_Pin_2))
+  {
+    Mode = 0;// Goes to the pre-defined reference value stored
+  }
+  else
+  {
+    Mode = 2;// Follows an external voltage source
+  }
+  if(currentmode == Mode)// If the modes are the same, it hasn't changed
+  {
+    return false;
+  }
+  else// Otherwise they have
+  {
+    return true;
+  }
+}
 //END FUNCTIONS//
