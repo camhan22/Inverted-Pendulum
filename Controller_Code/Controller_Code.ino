99fdf6ac37d726d59e5d8f2a1dcb73f8ea1bed53
camhan22 <hansoncameron18@yahoo.com>
2020-06-03 05:41:19 -0400

    Changed the sample time
    The sample time was too slow for thr system which made it freak out sometimes when the swing would startup

--- Controller_Code/Controller_Code.ino
+++ Controller_Code/Controller_Code.ino
@@ -15,7 +15,7 @@
  */
 
 //PIN DEFINITIONS//
-int PWM_Pin = 11;
+int PWM_Pin = 10;
 int IN3_Pin = 2;
 int IN4_Pin = 3;
 int Power_Pin = 13;// Pin used to provide power to the H-Bridge for convience
@@ -31,23 +31,23 @@ int Track_Pin = A3;
 //SYSTEM PARAMETERS//
 int Max_Sensor_Value = 1023;// Max sensor value at system limits
 int Min_Sensor_Value = 0;// Min sensor value at system limts
-int Sample_Period = 50;// Time between samples
+int Sample_Period = 10;// Time between samples
 unsigned long Current_Time = 0;
 unsigned long Last_Time = 0;
 int Mode = 0;
 //END SYSTEM PARAMETERS//
 
 //CONTROLLER PARAMETERS//
-double Control_Signal = 25 ;
+double Control_Signal = 20;
 int Current_Value;
-int Reference = 300;
+int Reference = 400;
 int Previous_Value;// Holds the previous value of the sensor during the swing
 bool is_Controlled = false;// Variable to hold whether the system should swing or control the arm
 bool is_Updated = false;// Tells if the swing direction and speed need to be updated
 //By default it should swing since it will be at the bottom of the arc
-int Swing_Limit = 10;// Variable to hold the value at which the controller will switch to controlled Mode
+int Swing_Limit = 15;// Variable to hold the value at which the controller will switch to controlled Mode
 int Swing_Rate = 5;// Dictates how fast the system will get to the controlling point
-Pid Controller(100, 'm', Reference, 1.5, 0.40, 0.2);// Create the controller
+Pid Controller(Sample_Period, 'm', Reference, 1.5, 0.40, 0.2);// Create the controller
 //END CONTROLLER PARAMETERS//
 
 //FUNCTION PROTOTYPES//
@@ -60,6 +60,7 @@ void(* resetFunc) (void) = 0;// Needed to reset the arduino by software
 
 void setup() {
   delay(2000);
+  Serial.begin(9600);
   //Setup the I/O pins
   pinMode(Power_Pin, OUTPUT);
   digitalWrite(Power_Pin, HIGH);// Set the pin to output 5v for H_Bridge
@@ -116,14 +117,14 @@ void setup() {
       Current_Value = analogRead(Angle_Sensor_Pin);
 		  //CHECK DIRECTION AND SEE IF UPDATE IS NEEDED//
 		  if(Control_Signal > 0){// If we are rotating in the clockwise direction
-			  if(Current_Value - Previous_Value <= 0 && Current_Value < (Max_Sensor_Value + Min_Sensor_Value)/2 && abs(Current_Value-Previous_Value) < 100){
+			  if(Current_Value - Previous_Value <= 0 && Current_Value < (Max_Sensor_Value + Min_Sensor_Value)/2 && abs(Current_Value-Previous_Value) < 10){
 				  // If the new value is less than the old value which can only happen when the direction reverses due to gravity, reverse the direction.
 				  //Also check to see that the difference is not greater than 200 which happens at the bottom of the swing due to crossover
 				  is_Updated = false;
 			  }
 		  }
 		  else{// Counter-clockwise direction
-			  if(Current_Value - Previous_Value >= 0 && Current_Value > (Max_Sensor_Value + Min_Sensor_Value)/2 && abs(Current_Value-Previous_Value) < 100){
+			  if(Current_Value - Previous_Value >= 0 && Current_Value > (Max_Sensor_Value + Min_Sensor_Value)/2 && abs(Current_Value-Previous_Value) < 10){
 				  is_Updated = false;// We have given it new commands and we shoudl do them
 			  }
 		  }
@@ -134,6 +135,7 @@ void setup() {
 			    Control_Signal+= (Control_Signal > 0) ? Swing_Rate : -1*Swing_Rate;
 			    // If the value of C_S is above zero, we add swing rate, if less than zero, we subtract. This ensures that we are increasing the power each time
 			    Control_Signal *= -1;// Reverse swing direction
+         Serial.println(Control_Signal);
 			    SetDirection(Control_Signal);
 			    SetSpeed(Control_Signal);
 			    is_Updated = true;// Tell the controller the state has been updated
@@ -144,13 +146,17 @@ void setup() {
 		  }
 	  }
 
-    // Check this every time through the loop, we do not want to miss this 
-    int Value = analogRead(Angle_Sensor_Pin);
-    if (Value > Reference-Swing_Limit && Value < Reference+Swing_Limit){// Check to see if it is within the limit for PID to take over
-    is_Controlled = true;// If so, then we will allow the PID to take over
-    SetSpeed(0);// Stop the motor
-    break;// And we need to exit the loop
-	  }
+    if(millis-Last_Time >= 1)
+    {
+      // Check this every time through the loop, we do not want to miss this 
+      int Value = analogRead(Angle_Sensor_Pin);
+      Serial.println(Value);
+      if (Value > Reference-Swing_Limit && Value < Reference+Swing_Limit){// Check to see if it is within the limit for PID to take over
+      is_Controlled = true;// If so, then we will allow the PID to take over
+      SetSpeed(0);// Stop the motor
+      break;// And we need to exit the loop
+	    }
+    }
   //If we have not gotten a sample, continue to do the same thing. i.e. change nothing
   }
   //END START-UP SWING//
